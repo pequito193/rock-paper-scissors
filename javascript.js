43d8e81493485e571ddbce0d32362492465b970b
pequito193 <vitorspequito@gmail.com>
2022-07-16 10:59:07 +0100

    Fix playRound and game functions

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 const possibleChoices = ['rock', 'paper', 'scissors'];
+let scoreComputer = 0;
+let scorePlayer = 0;
 
 function promptPlayer() {
     let playerChoice = prompt("Rock,Paper,Scissors? ");
     let lowerCasePlayerChoice = playerChoice.toLowerCase();
-    if (possibleChoices.includes(lowerCasePlayerChoice))
+    if (possibleChoices.includes(lowerCasePlayerChoice)) {
         return lowerCasePlayerChoice;
-    else
+    }else {
         console.log(lowerCasePlayerChoice + ' is not an option!');
+    }
 }
 
 function computerPlay() {
@@ -18,33 +21,54 @@ function computerPlay() {
 function playRound(lowerCasePlayerChoice, computerChoice) {
     promptPlayer();
     computerPlay();
-    if ((lowerCasePlayerChoice == 'rock' && computerChoice == 'scissors') || (lowerCasePlayerChoice == 'paper' && computerChoice == 'rock') || (lowerCasePlayerChoice == 'scissors' && computerChoice == 'paper'))
-            console.log('You won the round!');
-        else if ((lowerCasePlayerChoice == 'rock' && computerChoice == 'paper') || (lowerCasePlayerChoice == 'paper' && computerChoice == 'scissors') || (lowerCasePlayerChoice == 'scissors' && computerChoice == 'rock'))
-            console.log('You lost the round!');
-        else
-            console.log('Draw!');
+    switch(lowerCasePlayerChoice) {
+        case 'rock':
+            if(computerChoice == 'paper') {
+                console.log('You lost');
+                scoreComputer++;
+            } else if(computerChoice == 'scissors') {
+                console.log('You won');
+                scorePlayer++;
+            } else {
+                console.log('You drew');
+            }
+        break;
+        case 'paper':
+            if(computerChoice == 'scissors') {
+                console.log('You lost');
+                scoreComputer++;
+            } else if(computerChoice == 'rock') {
+                console.log('You won');
+                scorePlayer++;
+            } else {
+                console.log('You drew');
+            }
+        break;
+        case 'scissors':
+            if(computerChoice == 'rock') {
+                console.log('You lost');
+                scoreComputer++;
+            } else if(computerChoice == 'paper') {
+                console.log('You won');
+                scorePlayer++;
+            } else {
+                console.log('You drew');
+            }
+        break;
+    }
+    console.log('Player: ' + scorePlayer);
+    console.log('Computer: ' + scoreComputer);
 }
 
 function game() {
-    const n = 5;
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    let numberOfDraws = 0;
-    for(i = 0; i < n; i++)
+    for(i = 0; i < 5; i++)
         playRound();
-        if (console.log('You won the round!'))
-            scorePlayer++;
-            else if (console.log('You lost the round!'))
-                scoreComputer++;
-            else if (console.log('Draw!'))
-                numberOfDraws++;
     if (scoreComputer > scorePlayer)
-        console.log('You lost the game!' + scorePlayer + ' - ' + scoreComputer);
+        console.log('You lost the game! ' + scorePlayer + ' - ' + scoreComputer);
         else if (scoreComputer < scorePlayer)
-            console.log('You won the game!' + scorePlayer + ' - ' + scoreComputer);
+            console.log('You won the game! ' + scorePlayer + ' - ' + scoreComputer);
         else
-            console.log('You drew the game!' + scorePlayer + ' - ' + scoreComputer);
+            console.log('You drew the game! ' + scorePlayer + ' - ' + scoreComputer);
 }
 
 game();
